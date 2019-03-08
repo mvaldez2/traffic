3be82445a73aeef89cd3915b64a06496353f2147
mvaldez2 <38698523+mvaldez2@users.noreply.github.com>
2019-03-08 13:33:09 -0600

    huge file

--- dateTest.py
+++ dateTest.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 pd.options.display.max_columns = 50
     
     
-file = 'sample_2.csv' 
+file = 'Mish_2016_2_24_to_3_3.csv' #download the huge file
 f_signals = 'signals.csv'
 f_event_codes = 'event_codes.csv'
 
@@ -29,11 +29,11 @@ event_codes = pd.read_csv(f_event_codes)
 signals = pd.read_csv(f_signals)
 data = pd.read_csv(file)
 
-data.sort_values("Timestamp", inplace=True)
+data.sort_values("timestamp", inplace=True)
 
 
 
-data['Timestamp'] = pd.to_datetime(data.Timestamp) #converts timestamp to datetime object
+data['timestamp'] = pd.to_datetime(data.timestamp) #converts timestamp to datetime object
 data['Signal'] = data['SignalID'].map(signals.set_index('SignalID')['Signal']) #adds signalid name column
 data['event'] = data['EventCodeID'].map(event_codes.set_index('code')['desc']) #adds event name column
 
@@ -51,16 +51,16 @@ print(data.head())
 #-----------
 car_count = data.loc[data.EventCodeID==82, :] #dataframe of occurrences of the detector being on
 
-car_count['Time'] = car_count.Timestamp.dt.time #time column
+car_count['Time'] = car_count.timestamp.dt.time #time column
 
 
 
-car_count.Timestamp.value_counts().sort_index()
+car_count.timestamp.value_counts().sort_index()
 
-car_count.set_index('Timestamp', drop=False, inplace=True)
-car_count.groupby(pd.Grouper(key='Timestamp', freq='15min')).count().plot(kind='bar', y='SignalID', figsize=(10,5)) #number of occurrences in a 15min interval
+car_count.set_index('timestamp', drop=False, inplace=True)
+car_count.groupby(pd.Grouper(key='timestamp', freq='15min')).count().plot(kind='bar', y='SignalID', figsize=(10,5)) #number of occurrences in a 15min interval
 
-car_count.Timestamp.dt.hour.value_counts().sort_index() #number of occurrences in an hour
+car_count.timestamp.dt.hour.value_counts().sort_index() #number of occurrences in an hour
 
 
 
@@ -69,18 +69,19 @@ car_count.Timestamp.dt.hour.value_counts().sort_index() #number of occurrences i
 
 #how to loop through dataframe
 #for index, row in data.iterrows():
-#    print(row['Timestamp'], row['Signal'])
+#    print(row['timestamp'], row['Signal'])
 
 #trying to get the duration of an event     
-signal = data.loc[data.Signal=='Beck @ CR17', :] 
+signal = data.loc[data.SignalID==327685, :] #gets one signal 
 
 
 
 
-cycle = signal.loc[signal['EventCodeID'].isin([1,7,8,9,10,11])]
+cycle = signal.loc[signal['EventCodeID'].isin([1,7,8,9,10,11])] #light phases in the signal
 
-lane = cycle.loc[(signal['Param'] == 6)]
+lane = cycle.loc[(signal['Param'] == 6)] #gets lane on a signal
+#figure out lane signal pattern  
 
-dur = lane.Timestamp.diff()
+dur = lane.timestamp.diff()
 lane['duration'] = dur.dt.seconds.div(60,fill_value=0)
-lane.plot(x='Timestamp', y='duration', style='o', figsize=(10,5))
+lane.plot(x='timestamp', y='duration', style='o', figsize=(10,5))
