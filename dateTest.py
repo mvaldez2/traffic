a84c3c961b99517dc7e408cf5fc31c15de5e1429
mvaldez2 <38698523+mvaldez2@users.noreply.github.com>
2019-03-18 17:53:51 -0500

    cycle lengths by lane

--- dateTest.py
+++ dateTest.py
@@ -72,8 +72,9 @@ car_count.timestamp.dt.hour.value_counts().sort_index() #number of occurrences i
 #trying to get the duration of an event     
 signal = data.loc[data.SignalID==327685, :] #gets one signal 
 
-signal.loc[data.EventCodeID==82, :].groupby(pd.Grouper(title='Signal Detector Counts',key='timestamp', freq='D')).count().plot(kind='bar', y='SignalID', figsize=(10,5))
+signal.loc[data.EventCodeID==82, :].groupby(pd.Grouper(key='timestamp', freq='D')).count().plot(title='Signal Detector Counts', kind='bar', y='SignalID', figsize=(10,5))
 
+#how do I check the split change for a lane?
 split = signal.loc[signal['EventCodeID'].isin([134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149])] #split events
 split.groupby(pd.Grouper(key='timestamp', freq='D')).count().plot(title='splits', kind='bar', y='SignalID', figsize=(10,5))
 
@@ -84,4 +85,30 @@ lane = cycle.loc[(signal['Param'] == 6)] #gets lane on a signal
 
 dur = lane.timestamp.diff()
 lane['duration'] = dur.dt.total_seconds().fillna(0).shift(-1)
-lane.plot(x='timestamp', y='duration', style='o', figsize=(10,5))
+
+
+#green ccycle length
+green_cycle = lane.loc[signal['EventCodeID'].isin([1,7])]
+green_cycle.plot(title='Green Cycle', x='timestamp', y='duration', figsize=(10,5), color='g')
+green_cycle.duration.describe() #5 number summary of green cycle length
+green_cycle.loc[green_cycle.duration>green_cycle.duration.mean(), :] #times the duration is greater than average
+
+#yellow cycle
+yellow_cycle = lane.loc[signal['EventCodeID'].isin([8,9])]
+yellow_cycle.plot(title='Yellow Cycle',x='timestamp', y='duration', figsize=(10,5), color='y')
+yellow_cycle.duration.describe() #5 number summary of yellow cycle length
+yellow_cycle.loc[yellow_cycle.duration>yellow_cycle.duration.mean(), :] #times the duration is greater than average
+
+#red cycle
+red_cycle = lane.loc[signal['EventCodeID'].isin([10,11])]
+red_cycle.plot(title='Red Cycle',x='timestamp', y='duration', figsize=(10,5), color='r')
+red_cycle.duration.describe() #5 number summary of red cycle length
+red_cycle.loc[red_cycle.duration>red_cycle.duration.mean(), :] #times the duration is greater than average
+
+
+#graph green and not green (binary)
+
+
+#for green arrival we would have to check if the detector is on during the time period that the light was green
+#do we use correlation somehow? 
+#create a function that takes timestamp and timeperiod, then get ocurrences that way
