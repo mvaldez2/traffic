b29b012854a617a52fcd87ce1a0f58c694677176
mvaldez2 <38698523+mvaldez2@users.noreply.github.com>
2019-05-08 18:55:27 -0500

    Add files via upload

--- traffic_functions_import.py
+++ traffic_functions_import.py
@@ -11,68 +11,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import step, show
 from tkinter.filedialog import askopenfilename
-
-#-------------------- Importing data ------------------------------------------    
+  
 file = askopenfilename() #get imported data
-
-#turn file into dataframe
-data = pd.read_csv(file)
+data = pd.read_csv(file) #turn file into dataframe
 data['Timestamp'] = pd.to_datetime(data.Timestamp)
 
 
+'''
+sets time period
+data : dataframe
+start_date: start date
+end_time: end date
+start: start time
+end: end time
+usage:
+    If you want to set a time period from the dataset you can set the function 
+    to a variable and use that as the data paremeter for the rest of the functions
+    ex:
+        df = time_period(data, "2019-04-29", "2019-04-30", "2:00", "4:00")
+        traffic(df,"CR6 @ CR17", 15)
+'''
+def time_period(data, start_date, end_date, start_time, end_time):
+    data.loc[(data.date==start_date) | (data.date==end_date)]
+    return data.set_index('Timestamp').between_time(start_time,end_time).reset_index()
+   
 
-print(data[['date', 'Signal', 'event','Param']])
-
-#sets signal from dataframe
-#data : dataframe
-#signal_name: name of signal
-#ex: set_signal(data,"CR6 @ CR17")
-def set_signal(data, signal_name):
-    return data.loc[data.Signal==signal_name, :]
-
-#sets date from dataframe
-#data : dataframe
-#selected_date: selected date
-#ex: set_date(data,"2019-04-03")
-def set_date(data, selected_date):
-    return data.loc[data.date==selected_date, :]  
-
-#sets range of dates from dataframe
-#data : dataframe
-#start: start date
-#end: end date
-#ex: date_range(data,"2019-04-03", "2019-04-09")
-def date_range(data, start, end):
-    return data.loc[(data.date==start) | (data.date==end)]
-
-#sets range of time from dataframe
-#data : dataframe
-#start: start time
-#end: end time
-#ex: time_range(data,"7:00", "12:00")
-def time_range(data, start, end):
-    return data.set_index('Timestamp').between_time(start,end).reset_index()
-    
 #-------------------------Counts-----------------------------------------------
+'''
 #returns bar graph of traphic density of a signal per 15 min
-#data : dataframe
-#signal_name: name of signal
-#ex: traffic(data,"CR6 @ CR17")
-def traffic(data, signal_name):
+data : dataframe
+signal_name: name of signal
+freq: frequency in minutes
+ex: traffic(data,"CR6 @ CR17", 15)
+'''
+def traffic(data, signal_name, freq):
     signal = data.loc[data.Signal==signal_name, :]
     vehicle_count = signal.loc[signal.EventCodeID==82, :] #dataframe of occurrences of the detector being on
-    vehicle_count['Time'] = vehicle_count.Timestamp.dt.time #time column 
     vehicle_count.Timestamp.value_counts().sort_index() #sorts data by timestamp 
     vehicle_count.set_index('Timestamp', drop=False, inplace=True)
-    group = vehicle_count.groupby(pd.Grouper(key='Timestamp', freq='15min')) #groups occurrences in a 15min interval
+    group = vehicle_count.groupby(pd.Grouper(key='Timestamp', freq= str(freq)+'min')) #groups occurrences in a 15min interval
+    group.count().plot(title=signal_name, kind='bar', y='event', figsize=(15,5), colormap='Paired')
+
+'''
+returns bar graph of traphic density of a lane on a signal per 15 min 
+data : dataframe
+signal_name: name of signal
+freq: frequency in minutes
+*args: parameters to get the phase of the signal 
+ex: traffic(data,"CR6 @ CR17, 15, 25, 40")
+'''
+def traffic_lane(data, signal_name, freq, *lane):
+    signal = data.loc[data.Signal==signal_name, :]
+    vehicle_count = signal.loc[(signal.EventCodeID==82) & (signal['Param'].isin(lane))] #dataframe of occurrences of the detector being on
+    vehicle_count.Timestamp.value_counts().sort_index() #sorts data by timestamp 
+    vehicle_count.set_index('Timestamp', drop=False, inplace=True)
+    group = vehicle_count.groupby(pd.Grouper(key='Timestamp', freq= str(freq)+'min')) #groups occurrences in a 15min interval
     group.count().plot(title=signal_name, kind='bar', y='event', figsize=(15,5), colormap='Paired')
     
- #------------------------ light cycles ---------------------------------------- 
-#returns graph of cycle lengths in seconds for traffic lights 
-#data : dataframe
-#signal_name: name of signal
-#phase: phase of traffic light
-#ex: cycle_length(data,"CR6 @ CR17", 2)
+#------------------------ light cycles ---------------------------------------- 
+'''
+returns graph of cycle lengths in seconds for traffic lights 
+data : dataframe
+signal_name: name of signal
+phase: phase of traffic light
+ex: cycle_length(data,"CR6 @ CR17", 2)
+'''
 def cycle_length(data, signal_name, phase):
     signal = data.loc[data.Signal==signal_name, :]
     cycle = signal.loc[(signal['EventCodeID'].isin([1,7,10,11])) & (signal['Param'] == phase)] #light phases in the signal
@@ -94,15 +97,17 @@ def cycle_length(data, signal_name, phase):
     red_cycle.plot(title='Red Cycle',x='Timestamp', y='duration', figsize=(15,5), color='r')
     
 
- #---------------- Compare detection systems -----------------------------------
-#returns graph that compares the activty of a loop detector and a pod detector
-#data : dataframe
-#signal_name: name of signal
-#loop: loop detector
-#pod: pod detectpr
-#ex: compare(data,"CR6 @ CR17", 33, 8)    
-def compare(data, signal_name, loop, pod):
-    data = time_range(data, '04:30', '04:35')
+#---------------- Compare detection systems -----------------------------------
+'''
+returns graph that compares the activty of a loop detector and a pod detector
+data : dataframe
+signal_name: name of signal
+loop: loop detector
+pod: pod detectpr
+ex: compare(data,"CR6 @ CR17", 33, 8, '04:30', '04:35')
+'''    
+def compare(data, signal_name, loop, pod, start, end):
+    data = data.set_index('Timestamp').between_time(start,end).reset_index()
     compare = data.loc[(data['EventCodeID'].isin([81,82])) & (data.Signal==signal_name)] #finds when detectors are on and off
     print(compare[['Timestamp','event','Param']])
     plt.figure(figsize=(15,5))
@@ -122,64 +127,34 @@ def compare(data, signal_name, loop, pod):
     show()
    
 
-#----------------------- IN PROGRESS ---------------------------------------
- #--------------------- Green Arrival ----------------------------------- 
-#graph green and not green (binary)
-#for green arrival we would have to check if the detector is on during the time period that the light was green
-#only use loops #5
-#add 5 seconds
-time = time_range(data, '00:00', '23:25')
-green = time.loc[time['EventCodeID'].isin([1, 7]) & (time.Param==7)]
-det = time.loc[time['EventCodeID'].isin([82]) & (time.Param.isin([5, 30]))]
-arrival = green.append(det)
-arrival.sort_values("Timestamp", inplace=True)    
-
 #----------------- Split Failure ---------------------------------------------- 
 #check loop 1
-#if detector is on after red end clearence its a split failure
-#ex: split_failure(data, [25, 40], 4) 
-def split_failure(data, dets, phase):
-    lights = data.loc[data['EventCodeID'].isin([1, 7, 10, 11]) & (data.Param==phase)]
-    detectors = data.loc[data['EventCodeID'].isin([81,82]) & (data.Param.isin(dets))]
+'''
+returns graph that finds the amount of split failures on a given lane
+data : dataframe
+signal_name: name of signal
+phase: phase of traffic light
+*dets: vehicle detectors on the same lane
+ex: split_failure(data, "CR6 @ CR17", 4, 25, 40)
+''' 
+def split_failure(data, signal_name, phase, *dets):
+    data = data.loc[data.Signal==signal_name, :]
+    lights = data.loc[data['EventCodeID'].isin([1, 7, 10, 11]) & (data.Param==phase)] #get light events and phase
+    detectors = data.loc[data['EventCodeID'].isin([81,82]) & (data.Param.isin(dets))] #get detectors that are on the same lane
     split = lights.append(detectors)
     split.sort_values("Timestamp", inplace=True)    
-    pattern = np.asarray([11, 81])
+    pattern = np.asarray([11, 81]) #split failure pattern
     n_obs = len(pattern)
     split['split_failure'] = (split['EventCodeID']
                            .rolling(window=n_obs , min_periods=n_obs)
                            .apply(lambda x: (x==pattern).all())
-                           .astype(bool)             # All as bools
-                           .shift(-1 * (n_obs - 1))  # Shift back
-                           .fillna(False)            # convert NaNs to False
-                           )
+                           .astype(bool)             
+                           .shift(-1 * (n_obs - 1))  
+                           .fillna(False)            
+                           ) #counts the occurrences of the pattern
     split_count = split.loc[split.split_failure==True, :] #dataframe of occurrences of the detector being on
     split_count.Timestamp.value_counts().sort_index() #sorts data by timestamp 
     split_count.set_index('Timestamp', drop=False, inplace=True)
     group = split_count.groupby(pd.Grouper(key='Timestamp', freq='15min')).count() #groups occurrences in a 15min interval
     group.plot(title="Split Failures", kind='bar', y='event', stacked=True, figsize=(15,5), colormap='Paired')    
     group.plot.bar(stacked=True, figsize=(15,5));
-
-#testing stuff
-lights = data.loc[data['EventCodeID'].isin([1, 7, 10, 11]) & (data.Param==4)]
-detectors = data.loc[data['EventCodeID'].isin([81,82]) & (data.Param.isin([25, 40]))]
-split = lights.append(detectors)
-split.sort_values("Timestamp", inplace=True)    
-pattern = np.asarray([11, 81])
-n_obs = len(pattern)
-split['split_failure'] = (split['EventCodeID']
-                       .rolling(window=n_obs , min_periods=n_obs)
-                       .apply(lambda x: (x==pattern).all())
-                       .astype(bool)             # All as bools
-                       .shift(-1 * (n_obs - 1))  # Shift back
-                       .fillna(False)            # convert NaNs to False
-                       )
-split_count = split.loc[split.split_failure==True, :] #dataframe of occurrences of the detector being on
-split_count.Timestamp.value_counts().sort_index() #sorts data by timestamp 
-split_count.set_index('Timestamp', drop=False, inplace=True)
-group = split_count.groupby(pd.Grouper(key='Timestamp', freq='15min')).count() #groups occurrences in a 15min interval
-group.plot(title="Split Failures", kind='bar', y='event', stacked=True, figsize=(15,5))    
-
-sf_lane = pd.DataFrame(columns=['date','SR','NL','SL'])
-sf_lane =  pd.concat([pd.DataFrame([["2019-04-03",split_count.shape[0],10,21]], columns=sf_lane.columns),sf_lane]).reset_index(drop=True)   
-sf_lane.plot.bar(stacked=True, figsize=(15,5));
-        
